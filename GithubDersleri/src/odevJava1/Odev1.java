3492ab793b262478fe28896a89c9d6e3a7b283c1
MFethullahKoycu <mfethullahkoycu@gmail.com>
2022-12-08 17:48:04 +0100

    ilk dersin odevleri

---
+++ GithubDersleri/src/odevJava1/Odev1.java
@@ -0,0 +1,12 @@
+package odevJava1;
+
+public class Odev1 {
+
+	public static void main (String [] args) {
+		
+	
+	System.out.println("Merhabalar nasilniz efenim ");
+	
+	}
+	
+}
---
+++ GithubDersleri/src/odevJava1/Odev2.java
@@ -0,0 +1,10 @@
+package odevJava1;
+
+public class Odev2 {
+
+	public static  void main (String[] args) {
+		
+		System.out.println("Odevleeeer");
+		
+	}
+}
