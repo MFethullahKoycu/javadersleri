c935dd79c994b552103d2a69328b9ada5e026c04
MFethullahKoycu <mfethullahkoycu@gmail.com>
2022-12-08 20:22:44 +0100

    ilk dersin commiti

---
+++ GithubDersleri/ilkDers/Question1.java
@@ -0,0 +1,53 @@
+package assigment;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+
+		
+		byte sayi1 = 1,  sayi2 = 2, sayi3 = 3;
+		
+		short sayi4, sayi5, sayi6;
+		sayi4 = 1234;
+		sayi5 = 2345;
+		sayi6 = 3456;
+		
+		int sayi7, sayi8, sayi9;
+		sayi7 = 555555;
+		sayi8 = 666666;
+		sayi9 = 777777;
+		
+		long sayi10 = 111111111, sayi11 = 222222222, sayi12 = 333333333;
+		
+		
+		float sayi13 = 1.234567890F, sayi14 =2.345678901F, sayi15 = 3.4567890123F;
+		
+		double sayi16 = 1.234567890, sayi17 = 2.345678901, sayi18 = 3.456789012;
+		
+		
+		
+		System.out.println(sayi1);
+		System.out.println(sayi2);
+		System.out.println(sayi3);
+		System.out.println(sayi4);
+		System.out.println(sayi5);
+		System.out.println(sayi6);
+		System.out.println(sayi7);
+		System.out.println(sayi8);
+		System.out.println(sayi9);
+		System.out.println(sayi10);
+		System.out.println(sayi11);
+		System.out.println(sayi12);
+		System.out.println(sayi13);
+		System.out.println(sayi14);
+		System.out.println(sayi15);
+		System.out.println(sayi16);
+		System.out.println(sayi17);
+		System.out.println(sayi18);
+		
+		
+		
+		
+	}
+
+}
---
+++ GithubDersleri/ilkDers/Question2.java
@@ -0,0 +1,44 @@
+package assigment;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+
+		
+		byte altdeger;
+		byte ustdeger;
+		
+		altdeger = Byte.MIN_VALUE;
+		ustdeger = Byte.MAX_VALUE ;
+		
+		short altdeger1 = Short.MIN_VALUE, ustdeger1 = Short.MAX_VALUE;
+		
+		int altdeger2 = Integer.MIN_VALUE, ustdeger2 = Integer.MAX_VALUE;
+		
+		long altdeger3 = Long.MIN_VALUE , ustdeger3 = Long.MAX_VALUE;
+		
+		float altdeger4 = Float.MIN_VALUE, ustdeger4 = Float.MAX_VALUE;
+		
+		double altdeger5 = Double.MIN_VALUE, ustdeger5 = Double.MAX_VALUE;
+		
+		
+		
+		
+		System.out.println(altdeger);
+		System.out.println(ustdeger);
+		System.out.println(altdeger1);
+		System.out.println(ustdeger1);
+		System.out.println(altdeger2);
+		System.out.println(ustdeger2);
+		System.out.println(altdeger3);
+		System.out.println(ustdeger3);
+		System.out.println(altdeger4);
+		System.out.println(ustdeger4);
+		System.out.println(altdeger5);
+		System.out.println(ustdeger5);
+		
+		
+		
+	}
+
+}
